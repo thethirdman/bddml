75da4904be53735916975b7c9ad50ae0c93130c9
ttm <Francois Ripault francois.ripault@epita.fr>
2013-03-26 17:38:44 +0100

    Add files for bddml

---
+++ src/8queens.ml
@@ -0,0 +1,49 @@
+open Printf
+module Tuple =
+  struct
+    type t = int * int
+    let compare = Pervasives.compare
+    let print (a,b) = printf "(%d,%d)" a b
+  end
+
+module M = BDD(Tuple)
+open M
+
+let rec make_seq = function
+  0 -> []
+  | n -> n::(make_seq (n-1))
+
+let make_indexes n =
+  let rec sub n = function
+    (i,j) when i > n -> []
+    | (i,j) when j == n -> (i,j)::(sub n (i+1,1))
+    | (i,j) -> (i,j)::(sub n (i,j+1)) in
+  sub n (1,1)
+
+let foldl1 fn lst = List.fold_left fn (List.hd lst) (List.tl lst)
+
+let invalid (x,y) (i,j) = ((x,y) <> (i,j))
+    && ((x+y==i+j) || (x-y==i-j) || (x==i) || (y==j))
+
+let in_sight n (x,y) =
+  let cannot_exist = foldl1 (&&.)
+    (List.map (fun a -> neg (singleton a))
+      (List.filter (invalid (x,y)) (make_indexes n))) in
+  (singleton (x,y)) => cannot_exist
+
+let doit n =
+  let queens = List.fold_left
+      (fun acc i -> acc &&. (List.fold_left
+        (fun acc j -> (singleton (i,j)) ||. acc) Zero (make_seq n)))
+        One (make_seq n)
+  in
+  List.fold_left (&&.) queens (List.map (in_sight n) (make_indexes
+  n))
+
+let print = function
+  None -> print_endline "No solutions"
+  | Some lst -> begin print_int (List.length lst); List.iter Tuple.print lst end
+
+let _ =
+  let bdd = doit 8 in
+  print (getSat bdd)
---
+++ src/bdd.ml
@@ -0,0 +1,147 @@
+module BDD = 
+  functor (Type :
+    sig
+      type t
+      val compare : t -> t -> int
+    end) ->
+struct
+(** ID PART *)
+type uid = int
+
+let uidMake =
+let cur = ref 2 in
+  (fun () -> (incr cur); !cur)
+
+(** BDD PART *)
+type 'a bdd = One | Zero
+  | Robdd of ('a bdd) * 'a * ('a bdd) * uid
+  | Robddref  of uid * 'a * uid * uid
+
+let identifier = function
+  | One -> 1
+  | Zero -> 0
+  | Robdd (_,_,_,id) -> id
+  | Robddref (_,_,_,id) -> id
+
+let eq a b = (identifier a) == (identifier b)
+
+let leafToBool = function
+  | One -> true
+  | Zero -> false
+  | _ -> raise (Invalid_argument "Fail to convert leaf to bool")
+
+let boolToLeaf = function
+  true -> One
+  | false -> Zero
+
+(** HASHTABLE PART *)
+type 'a robdd_id = uid * 'a * uid
+
+let context:((Type.t robdd_id,Type.t bdd) Hashtbl.t) = (Hashtbl.create 1023)
+
+
+let lookup elt =
+  if Hashtbl.mem context elt then
+    Some (Hashtbl.find context elt)
+  else
+    None
+
+let lookupUnsafe = Hashtbl.find context
+
+let insert id bdd = Hashtbl.add context id bdd
+
+let rec isSingleton = function
+  Robdd (l,_,r,_) when ((l == One) || (l == Zero)) &&
+    ((r == One) || (r == Zero)) -> true
+  | Robddref (left,v,right,_) -> isSingleton (lookupUnsafe (left,v,right))
+  | _ -> false
+
+let mkNode l v r =
+  let (lid,rid) = (identifier l,identifier r) in
+  match lookup (lid,v,rid) with
+    | Some id -> Robddref (lid,v,rid,(identifier id))
+    | None ->
+    if (eq l r) then l
+    else
+      begin
+        let res = Robdd (l,v,r,uidMake ()) in
+        Hashtbl.add context (lid,v,rid) res;
+        res
+      end
+
+let singleton v = mkNode Zero v One
+
+let rec unaryApply fn = function
+  Robdd (l,v,r,_) -> mkNode (unaryApply fn l) v (unaryApply fn r)
+  | Robddref (l,v,r,_) -> unaryApply fn (lookupUnsafe (l,v,r))
+  | rest -> boolToLeaf (fn (leafToBool rest))
+
+
+let rec apply fn left right =
+match (left,right) with
+  | (Zero, Robdd (l,v,r,_)) | (Robdd (l,v,r,_), Zero)
+    -> mkNode (apply fn Zero l) v (apply fn Zero r)
+  | (One, Robdd (l,v,r,_)) | (Robdd (l,v,r,_), One)
+    -> mkNode (apply fn One l) v (apply fn One r)
+  | (Robddref (l,v,r,_), other) -> apply fn (lookupUnsafe (l,v,r)) other
+  | (other, Robddref (l,v,r,_)) -> apply fn other (lookupUnsafe (l,v,r))
+  | (Robdd (l,v,r,_),Robdd (l',v',r',_))
+    -> begin
+        match compare v v' with
+        | 0 -> mkNode (apply fn l l') v (apply fn r r')
+        | n when n < 0 -> mkNode (apply fn l right) v (apply fn r right)
+        | _ -> mkNode (apply fn left l') v (apply fn left r')
+      end
+  | (l,r) -> boolToLeaf (fn (leafToBool l) (leafToBool r))
+
+let neg = unaryApply not
+let (&&.) = apply (&&)
+let (||.) = apply (||)
+let (^.)  = apply (<>)
+let (=>) a b = apply (fun a b -> (not a) || b) a b
+let (<=>) = apply (==)
+
+let rec getSat = function
+  Zero -> None
+  | One -> Some []
+  | Robdd (l,v,r,_) ->
+      begin
+        match getSat l with
+        Some c -> Some (v::c)
+        | None ->
+          begin
+            match getSat r with
+            Some c -> Some (v::c)
+            | None -> None
+          end
+      end
+  | Robddref (l,v,r,_) -> getSat (lookupUnsafe (l,v,r))
+
+type 'a value = True of 'a | False of 'a
+
+let rec getSatList = function
+  Zero -> []
+  | One -> [[]]
+  | Robdd (l,v,r,_) ->
+    (List.map (fun e -> (True v)::e) (getSatList l))
+    @(List.map (fun e -> (False v)::e) (getSatList r))
+  | Robddref (l,v,r,_) -> getSatList (lookupUnsafe (l,v,r))
+
+let rec restrict var value = function
+  | Robdd (l,v,r,_) ->
+    if v == var then
+      let dir = if value  then l else r in
+      restrict var value dir
+    else
+      mkNode (restrict var value l) v (restrict var value r)
+  | Robddref (l,v,r,_) -> restrict var value (lookupUnsafe (l,v,r))
+  | n -> n
+
+let exists' var bdd =
+  (restrict var false bdd) ||. (restrict var true bdd)
+
+let rec exists var bdd = match var with
+  | Robdd (l,v,r,_) when isSingleton var -> exists' v bdd
+  | Robddref (l,v,r,_) -> exists var (lookupUnsafe (l,v,r))
+  | _ -> raise (Invalid_argument "Exists: not a singleton")
+end
